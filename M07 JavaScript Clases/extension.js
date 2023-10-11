4b2251ceea242619cf7ee0ed46b6609632227d3a
ekinast <ekinast@gmail.com>
2023-10-11 19:23:37 -0300

    M7

---
+++ M07 JavaScript Clases/extension.js
@@ -0,0 +1,28 @@
+class Persona{
+    constructor(nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+
+    }
+
+    saludar(){
+        console.log('Hola, mi nombre es ' + this.nombre + '. Tengo ' + this.edad);
+    }
+}
+
+class Programador extends Persona{
+    constructor(nombre, edad, añosDeExperiencia) {
+        super(nombre, edad);
+        this.añosDeExperiencia = añosDeExperiencia;
+    }
+
+    codear(){
+        console.log('Soy ' + this.nombre + '. Codeo desde hace ' + this.añosDeExperiencia + ' años.')
+    }
+}
+
+var martin = new Persona('Martín', 26);
+var programador = new Programador('María', 37, 4);
+//martin.saludar();
+//programador.codear();
+programador.saludar();
